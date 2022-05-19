8aab32c2fedfde750c597f1110c4254fc2e13900
dngabo <dngabo@wys.rw>
2022-05-19 12:15:00 +0200

    adding jszip to reduce file size

--- src/script.js
+++ src/script.js
@@ -6,73 +6,102 @@ const container = document.querySelector("#container");
 
 const generatePdf = () => {
   try {
-    // window.open("http://127.0.0.1:5500/src/", "_blank").focus();
     fetch("https://jsonplaceholder.typicode.com/posts/1")
       .then((response) => response.json())
       .then((post) => {
         const html = `
-  <div id="element" class="bg-[url('background.jpg')] p-6">
-        <div class="p-2 bg-green-100">
-          <h3 class="uppercase text-green-600 text-lg text-center font-bold">
-            Grow my tree
-          </h3>
-
-          <h3
-            class="capitalize font-sans text-green-700 mt-6 text-4xl text-center font-bold"
-          >
-            tree certificate
-          </h3>
-          <p class="text-green-600 text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing.
-          </p>
-          <h3 class="capitalize text-green-600 text-lg text-center font-bold">
-            22 trees
-          </h3>
-          <p class="text-green-600 text-center">Lorem ipsum dolor sit amet.</p>
-          <div class="w-full flex items-center justify-between">
-            <div class="w-10/12">
-              <p class="mt-4">
-                ${post.body}
-              </p>
-
-              <p class="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                rem obcaecati quia quisquam eius alias, illo nesciunt facilis
-                iure aliquid doloremque sit quasi illum temporibus beatae quod
-                qui deserunt aperiam.
-              </p>
+        <div class="w-4/5 m-4 mx-auto">
+        <div id="element" class="bg-[url('background.jpg')] p-6">
+          <div class="p-2 bg-[#D5DFCC]">
+            <img
+              src="grow_my_tree.png"
+              class="w-2/5 mx-auto"
+              alt="grow my tree logo"
+            />
+  
+            <h3
+              class="capitalize font-sans text-[#1D2D1D] mt-6 text-5xl text-center font-bold"
+            >
+              Baum-Zertificat
+            </h3>
+  
+            <h4 class="text-[#6F894C] mt-4 text-center">Lorem, ipsum.</h4>
+            <p class="text-[#1D2D1D] mt-4 text-center">
+              hat
+              <span class="text-[#708C53] font-bold text-3xl">1 baum</span>
+              gepflanzt
+            </p>
+            <h3 class="capitalize mt-4 text-[#1D2D1D] text-lg text-center">
+              Lorem, ipsum dolor.
+            </h3>
+            <div class="w-full p-3 flex items-start justify-between">
+              <div class="">
+                <p class="mt-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  rem obcaecati quia quisquam eius alias, illo nesciunt facilis
+                  iure aliquid doloremque sit quasi illum temporibus beatae quod
+                  qui deserunt aperiam Lorem ipsum dolor sit amet consectetur..
+                </p>
+                <div class="mt-4">
+                  <p class="mt-4">
+                    rem obcaecati quia quisquam eius alias, illo nesciunt facilis
+                    iure aliquid:
+                  </p>
+                  <ul class="mt-8">
+                  <li class="text-[#6F894C] mt-4">
+                  <i class="fa-solid fa-check"></i>
+                  <span class="inline ml-4">Lorem ipsum dolor sit amet.</span>
+                </li>
+                <li class="text-[#6F894C] mt-4">
+                  <i class="fa-solid fa-check"></i>
+                  <span class="inline ml-4">Lorem ipsum dolor sit amet.</span>
+                </li>
+                <li class="text-[#6F894C] mt-4">
+                  <i class="fa-solid fa-check"></i>
+                  <span class="inline ml-4">Lorem ipsum dolor sit amet.</span>
+                </li>
+                <li class="text-[#6F894C] mt-4">
+                  <i class="fa-solid fa-check"></i>
+                  <span class="inline ml-4">Lorem ipsum dolor sit amet.</span>
+                </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="">
+                <p class="mt-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  rem obcaecati quia quisquam eius alias, illo nesciunt facilis
+                  iure aliquid doloremque sit quasi illum temporibus beatae quod
+                  qui deserunt aperiam.
+                </p>
+                <p class="mt-4">
+                  ${post.body}
+                </p>
+  
+                <p class="mt-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  rem obcaecati quia quisquam eius alias, illo nesciunt facilis
+                  rem obcaecati quia quisquam eius alias, illo nesciunt facilis
+                  iure aliquid doloremque sit quasi illum temporibus beatae quod
+                  qui deserunt aperiam.
+                </p>
+              </div>
             </div>
-            <div class="w-10/12">
-              <p class="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                rem obcaecati quia quisquam eius alias, illo nesciunt facilis
-                iure aliquid doloremque sit quasi illum temporibus beatae quod
-                qui deserunt aperiam.
-              </p>
-              <p class="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                rem obcaecati quia quisquam eius alias, illo nesciunt facilis
-                iure aliquid doloremque sit quasi illum temporibus beatae quod
-                qui deserunt aperiam.
-              </p>
-            </div>
-          </div>
-
-          <div class="w-full mt-20 flex items-center justify-between">
-            <div>
-              <ul>
-                <li>Lorem, ipsum dolor.</li>
-                <li>Lorem, ipsum dolor.</li>
-                <li>Lorem, ipsum dolor.</li>
-              </ul>
-            </div>
-            <div>
-              <p class="mt-4">Lorem ipsum</p>
-              <p class="mt-4">Lorem ipsum dolor</p>
+  
+            <div class="w-full mt-20 flex items-center justify-between">
+              <div>
+                <p>Lorem, ipsum.</p>
+                <p>CEO/FOUNDER</p>
+                <p class="underline text-[#6F894C]">growmytree.com</p>
+              </div>
+              <div>
+                <p class="mt-4 text-[#6F894C]">Berlin,20.07.2021</p>
+                <img src="signature.png" class="w-52" alt="signature" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </div>          
   `;
         container.innerHTML = html;
         download();
@@ -91,14 +120,24 @@ const opt = {
   },
   jsPDF: {
     unit: "px",
-    format: [598, 760],
+    format: [698, 1060],
     hotfixes: ["px_scaling"],
     orientation: "portrait",
   },
 };
 
 const download = () => {
-  html2pdf().set(opt).from(element).save();
+  //html2pdf().set(opt).from(element).save();
+
+  var zip = new JSZip();
+  zip.file("certificate.pdf", html2pdf().from(element).set(opt).output("blob"));
+  zip
+    .generateAsync({
+      type: "blob",
+      compression: "DEFLATE",
+      compressionOptions: { level: 9 },
+    })
+    .then((res) => saveAs(res, "GMTCertificate.zip"));
 };
 
 button.addEventListener("click", () => {
